1493616f5e3d78aa3af2c646f30bd91b0a04a614
Beto <beto_2613@hotmail.com>
2016-12-20 17:33:44 +0100

    Find Elements
    +queryselector

--- src/bto.js
+++ src/bto.js
@@ -27,31 +27,6 @@ var bto = function(){
     else {params[pair[0]].push(decodeURIComponent(pair[1]));}
   }
   properties.params = params;
-
-
-  
-
-
-  /*
-   * Export Table by Id to Excel
-   * bto.exportToExcel('tableId', 'W3C Example Table');
-   */
-  var exportExcel = (function() {
-    var uri = 'data:application/vnd.ms-excel;base64,'
-    , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
-    , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
-    , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
-    return function(table, name) {
-      if (!table.nodeType) table = document.getElementById(table)
-      var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
-      window.location.href = uri + base64(format(template, ctx))
-    }
-  })();
-  methods.exportToExcel = exportExcel;
-  
-  
-  
-  
   
   /*
    * Detect Navigator and Version
@@ -79,7 +54,34 @@ var bto = function(){
   })();
   properties.navigator = navigator.getnameversion;
 
-  
+  /*
+   * Export Table by Id to Excel
+   * bto.exportToExcel('tableId', 'W3C Example Table');
+   */
+  var exportExcel = (function() {
+    var uri = 'data:application/vnd.ms-excel;base64,'
+    , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+    , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
+    , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
+    return function(table, name) {
+      if (!table.nodeType) table = document.getElementById(table)
+      var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+      window.location.href = uri + base64(format(template, ctx))
+    }
+  })();
+  methods.exportToExcel = exportExcel;
+
+  /*
+   * Find elements
+   * bto.exportToExcel('tableId', 'W3C Example Table');
+   */
+  var querySel = (function() {
+    return function(queryStr){
+      var selector = document.querySelectorAll(queryStr);
+      return (selector.length>0 ? (selector.length>1 ? selector : selector[0] ) : null );
+    }
+  })();
+  methods.find = querySel;
   
   //ADD PROPERTIES AND METHODS TO CLASS
   classBTO.propertie = properties;
